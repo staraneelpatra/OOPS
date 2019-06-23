00705f878c10f135a526d3e0aa0b613550e8a4b9
staraneel <33164249+staraneelpatra@users.noreply.github.com>
2019-06-23 20:18:56 +0530

    pusk

--- OOPS/Output.cs
+++ OOPS/Output.cs
@@ -1,38 +1,33 @@
 ﻿using System;
 namespace OOPS
-{
+{ public class Parent
+    {
+    public virtual void Add(int y ,int u)
+    {
+
+            Console.WriteLine(y+u);
+
+        }
+    }
+     public class Small:Parent
+    {
+       sealed public override void Add(int y, int u)
+        {
+            Console.WriteLine(y - u);
+        }
+    }
+
+    public class Tiny : Parent
+    {
+        public override void Add(int y, int u)
+        {
+            Console.WriteLine(y * u);
+        }
+    }
     public class Output
     {
+
         public static void Main(){
-
-            int x = 0;
-            int y = 0;
-
-            for (short z = 0; z < 3; z++)
-            {
-                if ((x++ > 1) || (y++ > 1))
-                {
-                    x++;
-                }
-            }
-            Console.WriteLine(x + "" + y);
-            Console.ReadKey();
-
-            int i = (byte)+(char)-(int)+(long)-1;
-            Console.WriteLine(i);
-
-
-            Console.WriteLine(1);
-            http://csharpStar.com/
-            Console.WriteLine(2);
-
-            int i1 = 13;
-            int j1 = 14;
-            i1 = --i1 - i1--;
-            j1 = ++j1 + j1++;
-            Console.WriteLine(i1 + j1);
-            Console.ReadKey();
-
 
         }
     }
