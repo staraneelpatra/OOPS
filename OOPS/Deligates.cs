b3e0ce0e416c6c5bd8f8e328e3d95de9e9cd0f9d
staraneel <33164249+staraneelpatra@users.noreply.github.com>
2019-10-20 21:19:41 +0530

    jhg

---
+++ OOPS/Deligates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace OOPS
+{
+    public delegate int Counting(int x, int y);
+    public class Deligates
+    {
+        public  int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Prod(int p, int q)
+        {
+            return p * q;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Deligates d = new Deligates();
+            Counting c = new Counting(d.Sum);
+            Console.WriteLine(c.Invoke(3, 4));
+            Console.WriteLine();
+
+            Counting c1 = null;
+            c1 += d.Sum;
+            c1 += d.Prod;
+            c.Invoke(2, 3);
+
+            
+        }
+    }
+}
---
+++ OOPS/WordCount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace OOPS
+{
+    public class WordCount
+    {
+        public virtual void show()
+        {
+            Console.WriteLine();
+        }
+        public static void Main(string[] args)
+        {
+            String Sentc = " I am a human being who loves to enjoy life ";
+            int count = 1;
+            Sentc = Sentc.Trim();
+            for (int i = Sentc.Length-1; i >= 0; i--)
+            {
+                if (Sentc[i]==' ')
+                {
+                    count += 1;
+                }
+            }
+            Console.WriteLine("Number of Words is {0}", count);
+           Console.ReadKey();
+        }
+    }
+    abstract class AA
+    {
+        public void Display()
+        {
+            Console.WriteLine();
+        }
+        public abstract void Print();
+    }
+}
