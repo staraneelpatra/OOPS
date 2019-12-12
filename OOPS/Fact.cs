0792be5543cbca1f23f862addd9f67a08a97e65c
staraneel <33164249+staraneelpatra@users.noreply.github.com>
2019-12-12 07:48:10 +0530

    b

---
+++ OOPS/Fact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OOPS
+{
+    public class Fact
+    {
+        public static void Main()
+        {
+            double x;
+            x = 0.1 + 0.2 - 0.3;
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
--- OOPS/ReadOnly.cs
+++ OOPS/ReadOnly.cs
@@ -4,24 +4,48 @@ namespace OOPS
 {
     class Age
     {
-        readonly int year;
-
-        public Age(int year){
-            this.year = year;
-            year = 321;
+       internal int MyProperty { get; set; }
+        public static bool s=true;
+        public static string name { get; set; }
+        public int j;
+        public int tim { get; set; }
+        readonly int year=9;
+        const int p = 4;
+        const DateTime today=DateTime.Now;
+        public  Age()
+        {
+            p= 3;
+            year = 10;
+            s = false;
+            name = "smile";
         }
-
-        public Age(){
-            year = 32;
+         static Age()
+        {
+            s = true;
+            name = "POKOri";
+            j = 9;
         }
+        public Age(int x)
+        {
+
+            p = x;
+            year = x;
+        }
+         public void display()
+        {
+            year = 4;
+        }
+        
     }
     class MainClass
     {
         public static void Main(string[] args)
-        {
+        { 
             Age age = new Age(32);
-
-            Console.WriteLine("Hello World!");
+            Age.name = "Anil";
+            var  yt=Age.s;
+            int po = age.j;
+            var t=age.MyProperty;
         }
     }
 }
